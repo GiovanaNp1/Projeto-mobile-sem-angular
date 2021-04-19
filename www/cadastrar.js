87de72aa1704ebc4ba7c2526ceb95faac90667f8
Giovana do Nascimento Pena <Giovana.np1@gmail.com>
2021-04-19 19:31:52 -0300

    cadastro screen

--- www/cadastrar.js
+++ www/cadastrar.js
@@ -1,19 +1,30 @@
+const dadosCadastro = [{
+  nome: '',
+  email: '',
+  senha: '',
+}]
+
 function confirmarSenhas(params) {
     var todo = document.getElementById("password");
     var todo2 = document.getElementById("password-again");
-
-    if(todo != todo2) { 
-
+    if(todo.value != todo2.value) { 
+      todo2.classList.add("is-invalid");
+      document.getElementById("invalid-feedback").style.display = "flex";
+      document.getElementById("password-again-formgroup").classList.add("form-group-invalid")
+    }else{
+      console.log("Funfou")
+      dadosCadastro.nome = document.getElementById("nome").value
+      dadosCadastro.email = document.getElementById("email").value
+      dadosCadastro.senha = document.getElementById("password").value
+      cadastrarUsuario()
     }
 }
 
-
-
 function cadastrarUsuario(params) {
-    
-}
-
-{/* <input type="text" class="form-control is-invalid" id="validationServer04" placeholder="State" required>
-      <div class="invalid-feedback">
-        Please provide a valid state.
-      </div> */}
+  var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
+  db.transaction(function (tx) {
+     tx.executeSql('CREATE TABLE IF NOT EXISTS COVID (nome, data, telefone, bairro, estado, cep)');
+     tx.executeSql('INSERT INTO COVID (nome, data, telefone, bairro, estado, cep) VALUES (?,?,?,?,?,?)', 
+     [nome, data, telefone, bairro, estado, cep]);
+  });
+};
